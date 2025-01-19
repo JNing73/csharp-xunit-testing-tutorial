6850249873d4aaf25dc06f7f86affca976b5fa3d
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-01-20 11:12:34 +1300

    Draft up test for machinedataprocessor

---
+++ WiredBrainCoffee.DataProcessorTest/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing;
+
+public class MachineDataProcessorTests
+{
+    [Fact]
+    public void ShouldSaveCountPerCoffeeType()
+    {
+        var machineDataProcessor = new MachineDataProcessor();
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0))
+        };
+
+        machineDataProcessor.ProcessItems(items);
+
+        // Assert that the correct values will be written to the console
+    }
+}

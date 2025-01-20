c33effdac28a6ae084dcc4530f7c9300a7c4495b
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-01-21 12:18:29 +1300

    Use dependency injection to inject a stringwriter to test writing to console

--- WiredBrainCoffee.DataProcessorTests/Data/ConsoleCoffeeCountStoreTests.cs
+++ WiredBrainCoffee.DataProcessorTests/Data/ConsoleCoffeeCountStoreTests.cs
@@ -6,9 +6,13 @@ public class ConsoleCoffeeCountStoreTests
     [Fact]
     public void ShouldWriteOutputToConsole()
     {
-        var coffeeCountItem = new CoffeeCountItem("Cappuccino", 5);
-        var consoleCoffeeCountStore = new ConsoleCoffeeCountStore();
+        var item = new CoffeeCountItem("Cappuccino", 5);
+        var stringWriter = new StringWriter();
+        var consoleCoffeeCountStore = new ConsoleCoffeeCountStore(stringWriter);
 
-        consoleCoffeeCountStore.Save(coffeeCountItem);
+        consoleCoffeeCountStore.Save(item);
+
+        var result = stringWriter.ToString();
+        Assert.Equal(($"{item.CoffeeType}:{item.Count}{Environment.NewLine}"), result);
     }
 }
